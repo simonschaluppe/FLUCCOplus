2023cc8c8b7b02b1c484bde1afe42095f28509eb
simonschaluppe <simon.schneider@technikum-wien.at>
2021-01-26 13:49:41 +0100

    transformer function refactored

--- FLUCCOplus/transform.py
+++ FLUCCOplus/transform.py
@@ -4,12 +4,14 @@ from sklearn import preprocessing
 
 from FLUCCOplus.config import *
 
+from matplotlib.figure import Figure
+
 @log
-def transform(points:np.ndarray, hour_scale=8760): #output dimension is always 8760 hours
+def transform(support_points:list, hour_scale:int=8760): #output dimension is always 8760 hours
     """returns the trigonometric polynomial fitting a given set of support points"""
 
     h = hour_scale #hours of periodic timeframe
-
+    points = np.array(support_points)
     N = len(points) #Number of support points
     x = np.arange(0, h, h/N, dtype=np.float64) #position of support points
     y = points  # support points of the scaler
@@ -22,6 +24,21 @@ def transform(points:np.ndarray, hour_scale=8760): #output dimension is always 8
     reps = int(np.ceil(9000 / h)) #how often does the signal repeat in a year(9000 instead of 8760 to account for rounding
 
     return np.concatenate([scaler] * reps)[:8760] # copies the signal reps times and returns only the full year
+
+@log
+def plot(scaler, support_points, hour_scale=8760, view_scale=1.5):
+    p, h = support_points, hour_scale
+
+    x = np.arange(0, h, h/len(p))
+    xh = np.arange(0,8760,1)
+
+    fig = Figure()
+    ax = fig.add_subplot()
+    a, b = 0, int(h*view_scale)
+    ax.plot(x[a:b], p[a:b], "bo")
+    ax.plot(xh[a:b], scaler[a:b], "r")
+    return fig
+
 
 
 @logg
